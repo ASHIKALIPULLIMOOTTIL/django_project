8ee8eab8540b745a33670297ee81065d40056533
ashik ali <ashikaju01@gmail.com>
2025-02-15 02:38:53 +0530

    form and static files

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
+    if request.POST:
+        print(request.POST)
     return render(request,'create.html')
 def list(request):
+    
     return render(request,'list.html')
 def edit(request):
     return render(request,'edit.html')
